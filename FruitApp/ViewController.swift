571c5ee2ffe3a13010ed5d16fe4ee63b11a77fa6
Alina Ene <alina.c.ene@gmail.com>
2019-11-17 10:59:38 +0000

    Initial Commit

---
+++ FruitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FruitApp
+//
+//  Created by Alina Ene on 17/11/2019.
+//  Copyright © 2019 Alina Ene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
