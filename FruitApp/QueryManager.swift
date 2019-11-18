0bf993d6959bccf0a76c677bddfeeb0d095a7d11
Alina Ene <alina.c.ene@gmail.com>
2019-11-18 00:12:09 +0000

    created sendStat func

--- FruitApp/QueryManager.swift
+++ FruitApp/QueryManager.swift
@@ -8,50 +8,82 @@
 
 import UIKit
 
+enum StatEvent: String {
+    case load = "load"
+    case display = "display"
+    case error = "error"
+}
+
 class QueryManager {
     
     private let fruitListUrl = "https://raw.githubusercontent.com/fmtvp/recruit-test-data/master/data.json"
     private let statsUrl = " https://raw.githubusercontent.com/fmtvp/recruit-test-data/master/stats"
     
     var dataTask: URLSessionDataTask?
+    var statDataTask: URLSessionDataTask?
     typealias JSONDictionary = [String: Any]
-    typealias QueryResult = (FruitBasket?, String) -> Void
+    typealias FruitQueryResult = (FruitBasket?, String) -> Void
+    typealias StatQueryResult = (Bool, String) -> Void
     let defaultSession = URLSession(configuration: .default)
     var errorMessage = ""
     
-    func loadFruitList(completion: @escaping QueryResult) {
+    func loadFruitList(completion: @escaping FruitQueryResult) {
         dataTask?.cancel()
         
         if var urlComponents = URLComponents(string: fruitListUrl) {
-          guard let url = urlComponents.url else {
-            return
-          }
-        
-          dataTask = defaultSession.dataTask(with: url) { [weak self] data, response, error in
-            defer {
-              self?.dataTask = nil
+            guard let url = urlComponents.url else {
+                return
             }
             
-            if let error = error {
-              self?.errorMessage += "Oops: " + error.localizedDescription
-            } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
-              do {
-                let basket = try JSONDecoder().decode(FruitBasket.self, from: data)
-                DispatchQueue.main.async {
-                    completion(basket, self?.errorMessage ?? "")
+            dataTask = defaultSession.dataTask(with: url) { [weak self] data, response, error in
+                defer {
+                    self?.dataTask = nil
                 }
-              } catch let parseError as NSError {
-                self?.errorMessage += "JSONSerialization error: \(parseError.localizedDescription)\n"
-                return
-              }
+                
+                if let error = error {
+                    self?.errorMessage += "Oops: " + error.localizedDescription
+                } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                    do {
+                        let basket = try JSONDecoder().decode(FruitBasket.self, from: data)
+                        DispatchQueue.main.async {
+                            completion(basket, self?.errorMessage ?? "")
+                        }
+                    } catch let parseError as NSError {
+                        self?.errorMessage += "JSONSerialization error: \(parseError.localizedDescription)\n"
+                        return
+                    }
+                }
             }
-          }
-
-          dataTask?.resume()
+            
+            dataTask?.resume()
         }
     }
     
-    func sendStat() {
+    func sendStat(event: StatEvent, data: String, completion: @escaping StatQueryResult) {
+        statDataTask?.cancel()
         
+        if var urlComponents = URLComponents(string: statsUrl) {
+            let queryItemToken = URLQueryItem(name: "event", value: event.rawValue)
+            let queryItemQuery = URLQueryItem(name: "data", value: data)
+            urlComponents.queryItems = [queryItemToken, queryItemQuery]
+            guard let url = urlComponents.url else {
+                return
+            }
+            
+            statDataTask = defaultSession.dataTask(with: url) { [weak self] data, response, error in
+                defer {
+                    self?.statDataTask = nil
+                }
+                
+                if let error = error {
+                    completion(false, "Oops: " + error.localizedDescription)
+                } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                    completion(true, "")
+                }
+            }
+            
+            statDataTask?.resume()
+        }
     }
+    
 }
